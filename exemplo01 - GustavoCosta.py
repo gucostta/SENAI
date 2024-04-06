f97bd314e35eca9f327b8b39bb8084c8174e63c9
Gustavo <108738783+gucostta@users.noreply.github.com>
2024-04-06 15:42:03 -0300

    Add files via upload

---
+++ exemplo01 - GustavoCosta.py
@@ -0,0 +1,13 @@
+"""
+Este é um exemplo de comentario de multiplas linhas de python.
+Note que são tres aspas duplas no inicio e no final do comentario
+
+"""
+#Declara e inicializa as variaveis
+valor1 = 5; valor2 = 2
+
+#Realiza o cauculo
+soma = valor1 + valor2
+
+#Exibe o resultado
+print("O resultado final é: ", soma)
