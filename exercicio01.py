4a714422ac17cce44c8bf16655a4ae32ae51e6ec
Gustavo <108738783+gucostta@users.noreply.github.com>
2024-04-06 16:37:31 -0300

    Add files via upload

---
+++ exercicio01.py
@@ -0,0 +1,11 @@
+"""
+1. Faça um programa que peça dois números inteiros. Imprima a soma destes números
+na tela do computador.
+"""
+
+n1 = int(input("Digite um numero inteiro:"))
+n2 = int(input("Digite outro número inteiro:"))
+
+soma = n1 + n2
+
+print("A soma de n1 + n2 é:", soma)
