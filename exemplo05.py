f12ab94f8cc5656100c13a63b90ff6a0711c19b0
Gustavo <108738783+gucostta@users.noreply.github.com>
2024-04-06 15:43:19 -0300

    Add files via upload

---
+++ exemplo05.py
@@ -0,0 +1,16 @@
+nome = input("Digite seu nome:")
+idade = input("Digite sua idade:")
+
+print("Nome:", nome)
+print("Idade:", idade)
+
+num1 = int(input("Digite n1:"))
+num2 = int(input("Digite n2:"))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+num3 = float(input("Digite n3:"))
+print(num3)
