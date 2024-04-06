2d3d52918131c1c870269255299cfd82d6098e08
Gustavo <108738783+gucostta@users.noreply.github.com>
2024-04-06 16:50:16 -0300

    Add files via upload

---
+++ exercicio06.py
@@ -0,0 +1,14 @@
+"""
+6. Desenvolva um programa que leia um número inteiro e apresente o número
+informado, seguido do seu antecessor e do seu sucessor. Use variaveis para
+desenvolver o programa.
+"""
+
+n1 = int(input("Digite um numero inteiro:"))
+
+antecessor = n1 - 1
+sucessor = n1 + 1
+
+print("Numero digitado:", n1)
+print("Atecessor:", antecessor)
+print("Sucessor:", sucessor)
