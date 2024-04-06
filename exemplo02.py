f72f3da4c3fbd64c62f6fa7e6d0a6fb15242df04
Gustavo <108738783+gucostta@users.noreply.github.com>
2024-04-06 15:42:33 -0300

    Add files via upload

---
+++ exemplo02.py
@@ -0,0 +1,10 @@
+"""
+Esse trecho de código mostra como ler dados informados pelo usuario,
+com a utilização do método input(). Este método retorna a string informada pelo
+usuario
+"""
+
+#Obtem o nome e a idade do usuario
+nome = input("Digite seu nome:")
+idade = input("Digite sua idade:")
+print("Olá!" + nome + ", " + idade + "! Voce gosta de Python?") 
