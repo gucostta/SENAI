564de5918bc23df59c9be1ae3252c3c4c7801c09
Gustavo <108738783+gucostta@users.noreply.github.com>
2024-04-27 15:59:58 -0300

    Add files via upload

---
+++ exemplo03def.py
@@ -0,0 +1,6 @@
+def epar(x):
+    return(x % 2 == 0)
+
+print(epar(2))
+print(epar(3))
+print(epar(10))
---
+++ exemplo05def.py
@@ -0,0 +1,9 @@
+a1 = 5
+
+def muda_e_imprime():
+    a1 = 7
+    print(a1)
+
+print(a1)
+muda_e_imprime()
+print(a1)
